c69841ffaf33e8d21621ec5343b40d420df630ab
Arafian01 <arafianw@gmail.com>
2024-05-10 19:55:26 +0700

    Update 3.10

--- CV/script2.js
+++ CV/script2.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
-    $("#home").hide();
-    $("#profile").hide();
-    $("#skills").hide();
-    $("#experience").hide();
-    
+    // $('[id^="navbutton"]').click(function(){
+    //     // Mengambil ID tombol yang diklik
+    //     var buttonId = $(this).attr('id');
+    //     // Mengambil angka dari ID tombol
+    //     var pageNum = buttonId.match(/\d+/)[0];
+    //     // Menyembunyikan semua halaman
+    //     $('.page').hide();
+    //     // Menampilkan halaman yang sesuai dengan tombol yang diklik
+    //     $('#page' + pageNum).fadeIn();
+    //     // Menambahkan kelas 'z-2' ke halaman yang ditampilkan
+    //     $('#page' + pageNum).addClass('z-2');
+    //     $('#navbutton' + $(this).attr('id')).addClass('active');
+    //   });
+    $('[id^="navbutton"]').click(function(){
+        var buttonId = $(this).attr('id');
+        var pageNum = buttonId.match(/\d+/)[0];
+        
+        // Menambahkan kelas 'active' pada tombol yang diklik dan menghapusnya dari tombol lainnya
+        $('.btn-outline-warning').removeClass('active');
+        $(this).addClass('active');
+        
+        // Menambahkan kelas 'z-2' pada halaman yang ditampilkan dan menghapusnya dari halaman lainnya
+        $('.page').removeClass('z-2').hide();
+        $('#page' + pageNum).addClass('z-2').show();
+      });
 });
